745934237de8234840a7ea9551f469459d3f988f
dhirajCodes543 <d4dhirajbarnwal@gmail.com>
2026-08-30 17:42:28 +0530

    configured redux

--- 09MegaProject/src/App.jsx
+++ 09MegaProject/src/App.jsx
@@ -1,12 +1,41 @@
-
+import { useState, useEffect } from "react";
+import { useDispatch } from "react-redux";
+import authService from "./appwrite/auth";
+import { login, logout } from "./store/authSlice";
+import { Footer, Header } from "./components";
 
 function App() {
-  
-  return (
-  <>
-    
-  </>
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    authService
+      .getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login(userData));
+        } else {
+          dispatch(logout());
+        }
+      })
+      .finally(() => setLoading(false));
+  }, []);
+
+  return !loading ? (
+    <div className="min-h-screen flex flex-wrap content-between bg-gray-400">
+      <div className="w-full text-center block">
+          <Header/>
+          <main>
+
+          </main>
+          <Footer/>
+      </div>
+    </div>
+  ) : (
+    <div>
+      hi loading
+    </div>
   )
 }
 
-export default App
+export default App;
